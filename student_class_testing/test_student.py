def76fd3e005cd6a8ba2ea75d603e64085ba81f0
ChrisT-CC <cristaecc@gmail.com>
2022-01-19 15:28:02 +0000

    Refactor the code by using setUp method to create a student instance

--- student_class_testing/test_student.py
+++ student_class_testing/test_student.py
@@ -5,32 +5,45 @@ from student import Student
 class TestStudent(unittest.TestCase):
     """ Test case """
 
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def setUp(self):
+        """ create the setUp method """
+        print("setUp")
+        # create an instance of the student class
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        """ create the setUp method """
+        print("tearDown")
+
     def test_full_name(self):
         """ 1st test """
-        # create an instance of the student class
-        student = Student("John", "Doe")
-
+        print("full_name")
         # test the student instance to see if calling the full_name method
         # returns the expected value
-        self.assertEqual(student.full_name, "John Doe")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
         """ 2nd test """
-        # create an instance of the student class
-        student = Student("John", "Doe")
+        print("alert_santa")
         # call alert_santa() method
-        student.alert_santa()
+        self.student.alert_santa()
 
         # test to see if alert_santa() method returns the expected output
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
 
     def test_email(self):
         """ 3rd test """
-        # create an instance of the student class
-        student = Student("John", "Doe")
-
+        print("email")
         # test to see if email() method returns the expected output
-        self.assertEqual(student.email, "john.doe@email.com")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
 
 if __name__ == "__main__":
