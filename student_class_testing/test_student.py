d6a00c258b83f6109c2004ec0602a03026a96c3e
ChrisT-CC <cristaecc@gmail.com>
2022-01-19 13:53:30 +0000

    Create first test

--- student_class_testing/test_student.py
+++ student_class_testing/test_student.py
@@ -1,8 +1,18 @@
 import unittest
-from student import student
+from student import Student
 
 
 class TestStudent(unittest.TestCase):
+    """ Test case """
+
+    def test_full_name(self):
+        """ 1st test """
+        # create an instance of the student class
+        student = Student("John", "Doe")
+
+        # test the student instance to see if calling the full_name method
+        # returns the expected value
+        self.assertEqual(student.full_name, "John Doe")
 
 
 if __name__ == "__main__":
